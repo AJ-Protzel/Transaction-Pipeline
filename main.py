e095d2312bf224742d0082be7e73ed428907861b
Aj Protzel <ajprotzel@gmail.com>
2025-02-03 12:27:23 -0800

    used project from cs512
    now with UI

--- main.py
+++ main.py
@@ -1,60 +1,28 @@
-import os  # Importing the os module for interacting with the operating system
-import sys  # Importing the sys module for system-specific parameters and functions
-import shutil  # Importing the shutil module for file operations
-from cleaner import load_config, load_mappings, clean_csv, transform_csv  # Importing functions from cleaner.py
+"""
+Author: Adrien Protzel
 
-# Static paths
-CLEAN_FOLDER_PATH = os.path.join('Data', 'Clean')  # Path to the folder where cleaned files will be stored
-BAD_LINES_LOG_PATH = os.path.join(CLEAN_FOLDER_PATH, 'bad_lines.txt')  # Path to the log file for bad lines
-MAPS_FOLDER_PATH = os.path.join('Configs', 'Maps')  # Path to the folder containing mapping files
-DESCRIPTION_MAP_PATH = os.path.join(MAPS_FOLDER_PATH, 'Description_map.txt')  # Path to the description mapping file
-CATEGORY_MAP_PATH = os.path.join(MAPS_FOLDER_PATH, 'Category_map.txt')  # Path to the category mapping file
+This program sequentially calls other scripts to import, manage, and clean data.
+"""
 
-def process_csv_files(config_path):
-    """Process all CSV files in the specified folder."""
-    # Load the configuration from the JSON file
-    config = load_config(config_path)
-
-    # Load the description and category mappings
-    description_mappings = load_mappings(DESCRIPTION_MAP_PATH)
-    category_mappings = load_mappings(CATEGORY_MAP_PATH)
-
-    print(f"Processing: {config['FOLDER_PATH']}")
-
-    # Iterate over all files in the specified folder
-    for filename in os.listdir(config['FOLDER_PATH']):
-        if filename.lower().endswith('.csv'):  # Check if the file is a CSV file
-            file = os.path.join(config['FOLDER_PATH'], filename)
-
-            # Clean and transform the CSV file
-            clean_csv(file, config, BAD_LINES_LOG_PATH)
-            transform_csv(file, config, description_mappings, category_mappings, DESCRIPTION_MAP_PATH, CATEGORY_MAP_PATH)
-
-            # Generate a new filename and move the processed file to the clean folder
-            new_filename = f"{config['ACCOUNT']}_{config['BANK']}_{config['CARD']}_{filename}"
-            new_file_path = os.path.join(CLEAN_FOLDER_PATH, new_filename)
-            shutil.move(file, new_file_path)
-
-
-def run_cleaner(config_path):
-    """Run the cleaner program with the specified configuration file."""
-    process_csv_files(config_path)
+import subprocess
 
 if __name__ == "__main__":
-    # Define the folder containing configuration files
-    configs_folder = 'Configs'
+    # Csk user to import files and to which folder <Type>_<Bank>_<Card>
+    subprocess.run(["python", "file_importer.py"])
+    print("File Importing......Done")
 
-    # Check if the configs folder exists
-    if not os.path.exists(configs_folder):
-        sys.exit(1)  # Exit the program if the folder does not exist
+    # Cleans headers and merges multiple files in single bank account file
+    subprocess.run(["python", "file_merger.py"])
+    print("File Merging......Done")
 
-    # List all JSON configuration files in the configs folder
-    configs = [os.path.join(configs_folder, config) for config in os.listdir(configs_folder) if config.endswith('.json')]
+    # Removes, renames, adds, splits columns, merges into single clean file in Clean folder
+    subprocess.run(["python", "file_cleaner.py"])
+    print("File Cleaning......Done")
 
-    # Exit if no configuration files are found
-    if not configs:
-        sys.exit(1)
+    # Convert CSV to JSON
+    subprocess.run(["python", "CSV_to_JSON.py"])
+    print("CSV to JSON......Done")
 
-    # Run the cleaner for each configuration file
-    for config in configs:
-        run_cleaner(config)
+    # Convert JSONto CSV
+    subprocess.run(["python", "JSON_to_CSV.py"])
+    print("JSON to CSV......Done")
